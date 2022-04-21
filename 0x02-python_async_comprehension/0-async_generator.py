b9bf42eccb3efcde62661591decc73920acde758
Travor oguna <40546478+travis1999@users.noreply.github.com>
2022-04-21 21:25:31 +0300

    async_generator now waits one second

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,12 @@
 """Async generator"""
 
 import random
+import asyncio
 from typing import Generator
 
 
 async def async_generator() -> Generator[int, None, None]:
     """Generate random numbers"""
     for _ in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
