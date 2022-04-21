8b6a5ce22705b7ae243dc54e802842f135502f5f
Travor oguna <40546478+travis1999@users.noreply.github.com>
2022-04-21 21:31:26 +0300

    Correctly anotated async_comprehention

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Async comprehension"""
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> None:
+async def async_comprehension() -> List[float]:
     """Async comprehension"""
     return [i async for i in async_generator()]
